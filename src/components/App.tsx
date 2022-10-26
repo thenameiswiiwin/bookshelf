594c93da0ce57cd36eba140d98628f41fe536e6d
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-10-26 15:11:26 -0500

    created Modal for login and register using reach/dialog

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,29 @@
+import '@reach/dialog/styles.css'
+import { Dialog } from '@reach/dialog'
+import { useState } from 'react'
 import { Logo } from './Logo'
 
 function App() {
+  const [openModal, setOpenModal] = useState('none')
+
   return (
     <div>
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
-      <button onClick={() => console.log('clicked')}>Login</button>
-      <button onClick={() => console.log('clicked')}>Register</button>
+      <button onClick={() => setOpenModal('login')}>Login</button>
+      <button onClick={() => setOpenModal('register')}>Register</button>
+      <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
+        <button onClick={() => setOpenModal('none')} type="button">
+          Close
+        </button>
+        <h2>Login</h2>
+      </Dialog>
+      <Dialog aria-label="Registration form" isOpen={openModal === 'register'}>
+        <button onClick={() => setOpenModal('none')} type="button">
+          Close
+        </button>
+        <h2>Register</h2>
+      </Dialog>
     </div>
   )
 }
