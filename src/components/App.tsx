fd8b91c49168cbeb0739b30afa27eb10fd903af1
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-10-26 15:05:59 -0500

    render logo, title, and login/register buttons

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,14 @@
+import { Logo } from './Logo'
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div>
+      <Logo width="80" height="80" />
+      <h1>Bookshelf</h1>
+      <button onClick={() => console.log('clicked')}>Login</button>
+      <button onClick={() => console.log('clicked')}>Register</button>
+    </div>
+  )
 }
 
 export default App
