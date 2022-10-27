7422a09280feda0a2e17f4ee3d95ad34499655b4
Huy Nguyen <huyn.nguyen95@gmail.com>
2022-10-27 18:00:19 -0500

    added styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,29 +9,51 @@ function App() {
 
   const login = (formData: string) => console.log('login', formData)
   const register = (formData: string) => console.log('register', formData)
+
   return (
-    <div>
+    <div className="flex h-screen w-full flex-col items-center justify-center space-y-2">
       {/* Home */}
       <Logo width="80" height="80" />
-      <h1>Bookshelf</h1>
-      <button onClick={() => setOpenModal('login')}>Login</button>
-      <button onClick={() => setOpenModal('register')}>Register</button>
-      {/* Login Modal */}
-      <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
-        <button onClick={() => setOpenModal('none')} type="button">
-          Close
+      <h1 className="text-4xl font-semibold">Bookshelf</h1>
+      <div className="grid grid-cols-2 gap-3">
+        <button
+          className="rounded border-0 bg-blue-600 py-2.5 px-4 leading-none text-white"
+          onClick={() => setOpenModal('login')}
+        >
+          Login
         </button>
-        <h2>Login</h2>
-        <LoginForm onSubmit={login} buttonText="Login" />
-      </Dialog>
-      {/* Register Modal */}
-      <Dialog aria-label="Registration form" isOpen={openModal === 'register'}>
-        <button onClick={() => setOpenModal('none')} type="button">
-          Close
+        <button
+          className="rounded border-0 bg-neutral-100 py-2.5 px-4 leading-none text-gray-600"
+          onClick={() => setOpenModal('register')}
+        >
+          Register
         </button>
-        <h2>Register</h2>
-        <LoginForm onSubmit={register} buttonText="Register" />
-      </Dialog>
+        {/* Login Modal */}
+        <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
+          <button
+            className="padding-0 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full border border-solid border-white border-gray-500 bg-white leading-none"
+            onClick={() => setOpenModal('none')}
+            type="button"
+          >
+            <span aria-hidden>×</span>
+          </button>
+          <LoginForm onSubmit={login} buttonText="Login" />
+        </Dialog>
+        {/* Register Modal */}
+        <Dialog
+          aria-label="Registration form"
+          isOpen={openModal === 'register'}
+        >
+          <button
+            className="padding-0 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full border border-solid border-white border-gray-500 bg-white leading-none"
+            onClick={() => setOpenModal('none')}
+            type="button"
+          >
+            <span aria-hidden>×</span>
+          </button>
+          <LoginForm onSubmit={register} buttonText="Register" />
+        </Dialog>
+      </div>
     </div>
   )
 }
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,30 +1,46 @@
 interface LoginFormProps {
-  onSubmit: Function
+  onSubmit: any
   buttonText: string
 }
 
 function LoginForm({ onSubmit, buttonText }: LoginFormProps) {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    const { username, password } = e.target.elements
+    const target = e.target as typeof e.target & {
+      username: { value: string }
+      password: { value: string }
+    }
 
     onSubmit({
-      username: username.value,
-      password: password.value
+      username: target.username.value,
+      password: target.password.value
     })
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
+    <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+      <div className="mx-auto my-2.5 flex w-full max-w-xs flex-col">
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          className="rounded border border-solid border-white bg-neutral-100 py-2 px-3"
+          id="username"
+          type="text"
+        />
       </div>
-      <div>
+      <div className="mx-auto my-2.5 flex w-full max-w-xs flex-col">
         <label htmlFor="password">Password</label>
-        <input id="password" type="password" />
+        <input
+          className="rounded border border-solid border-white bg-neutral-100 py-2 px-3"
+          id="password"
+          type="password"
+        />
       </div>
-      <button type="submit">{buttonText}</button>
+      <button
+        className="rounded border-0 bg-blue-600 py-2.5 px-4 leading-none text-white"
+        type="submit"
+      >
+        {buttonText}
+      </button>
     </form>
   )
 }
